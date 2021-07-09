1fd106e15e031cc0e99c4168682db84535731d97
irvb <irvb@MacBook-Pro-de-irvb.local>
2021-07-09 18:45:54 +0200

    db config

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2fwcu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2fwcu.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`,
 { useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: true,
